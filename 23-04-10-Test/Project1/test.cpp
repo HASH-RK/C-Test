8643827a94903fc34f0d691e61c6040e9f0f9541
HASHRK <1850094305@qq.com>
2023-04-11 23:04:54 +0800

    hh

--- 23-04-10-Test/Project1/test.cpp
+++ 23-04-10-Test/Project1/test.cpp
@@ -3,15 +3,46 @@
 #include<cstring>
 #include<iostream>
 
+//void test(char** ppc)
+//{
+//
+//}
+
+//int main()
+//{
+//	//int arr[3][5];
+//	//printf("%p\n",arr);
+//	//printf("%p\n",arr + 1);
+//
+//	//printf("%p\n",&arr);
+//	//printf("%p\n",&arr + 1);
+//
+//
+//	char* arr[4];
+//	test(arr);
+//
+//	return 0;
+//}
+
+void test1(int(*p)[5])
+{}
+void test2(int(*p)[3][5])
+{
+	*p;
+}
 int main()
 {
-	int arr[3][5];
-	printf("%p\n",arr);
-	printf("%p\n",arr + 1);
+	//int arr[3][5];
+	//test1(arr);//传递的第一行的地址
+	//test2(&arr);//传递的是整个二维数组的地址
 
-	printf("%p\n",&arr);
-	printf("%p\n",&arr + 1);
+	//int* p2;
 
-
+	int (*p2)[10];
+	//printf("%p",p2);
+	for (int i = 0; i < 10; i++)
+	{
+	//	printf("%d", (*p2)[i]);
+	}
 	return 0;
 }
